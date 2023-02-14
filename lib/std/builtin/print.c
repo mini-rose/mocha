10cb18562beef9c0f694dc2fdd4ea4a1b86d13d3
bellrise <bellrise.dev@gmail.com>
2023-02-14 23:25:23 +0100

    std.builtin.print: move mangled calls into C
    Rather than using a "jump pad", these functions are directly implemented
    in C.

--- lib/std/builtin/print.c
+++ lib/std/builtin/print.c
@@ -5,17 +5,17 @@
 
 #include <stdio.h>
 
-cf_null cf_print_i32(cf_i32 num)
+cf_null _C5printi(cf_i32 num)
 {
 	printf("%d\n", num);
 }
 
-cf_null cf_print_i64(cf_i64 num)
+cf_null _C5printl(cf_i64 num)
 {
 	printf("%ld\n", num);
 }
 
-cf_null cf_print_str(struct cf_str *string)
+cf_null _C5printP3str(struct cf_str *string)
 {
 	printf("%.*s\n", (int) string->len, string->ptr);
 }
