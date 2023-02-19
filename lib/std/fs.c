dbd3c467db5d209e21ca8c18fee88de51cfe3007
fenze <contact@fenze.dev>
2023-02-19 14:16:30 +0100

    std.fs: add `is_dir` and `is_file` functions

--- lib/std/fs.c
+++ lib/std/fs.c
@@ -21,3 +21,27 @@ void _C5touchP3stri(cf_str *path, cf_i32 mode)
 	chmod(pstr, mode);
 	free(pstr);
 }
+
+cf_bool _C7is_fileP3str(cf_str *path)
+{
+	struct stat p_stat;
+	char *pstr = strndup(path->ptr, path->len);
+
+	if (stat(pstr, &p_stat) != 0)
+		return false;
+
+	free(pstr);
+
+	return (S_ISREG(p_stat.st_mode)) ? 1 : 0;
+}
+
+cf_bool _C6is_dirP3str(cf_str *path)
+{
+	struct stat p_stat;
+	char *pstr = strndup(path->ptr, path->len);
+
+	if (stat(pstr, &p_stat) != 0)
+		return false;
+
+	return S_ISDIR(p_stat.st_mode);
+}
