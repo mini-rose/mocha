55cab42150bcef1e4b4a84caef059e1d49c890b8
fenze <contact@fenze.dev>
2023-02-20 21:12:55 +0100

    stdio: change allocs to memcpy

--- lib/std/io.c
+++ lib/std/io.c
@@ -15,19 +15,25 @@ cf_null _write_stream(cf_i32 stream, cf_i8 *buf, cf_i64 len)
 	write(stream, buf, len);
 }
 
-cf_i8 *_open(cf_str *path, cf_str *mode)
+cf_i8 *_open(cf_str *_path, cf_str *_mode)
 {
-	char *p = strndup(path->ptr, path->len);
-	char *m = strndup(mode->ptr, mode->len);
+	char __path[_path->len];
+	char __mode[_mode->len];
+	memcpy(__path, _path->ptr, _path->len);
+	memcpy(__mode, _mode->ptr, _mode->len);
+	__path[_path->len] = '\0';
+	__mode[_mode->len] = '\0';
 
-	FILE *fp = fopen(p, m);
+	FILE *fp = fopen(__path, __mode);
 
-	free(p);
-	free(m);
 	return (cf_i8 *) fp;
 }
 
 cf_null _close(cf_i8 *file)
 {
+	if (file == NULL)
+		return;
+
 	fclose((FILE *) file);
+	file = NULL;
 }
